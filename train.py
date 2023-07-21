e98863f46de366babd84557101aed5f9593517dc
leob03 <87416230+leob03@users.noreply.github.com>
2023-07-21 01:10:41 -0400

    Update train.py

--- train.py
+++ train.py
@@ -9,9 +9,13 @@ from utils.grad import rel_error
 
 from two_stage_detector import RPNPredictionNetwork
 
-rpn_pred_net = RPNPredictionNetwork(
-    in_channels=64, stem_channels=[64], num_anchors=3
-)
+path = os.getcwd()
+PATH = os.path.join(path)
+
+if torch.cuda.is_available():
+    DEVICE = torch.device("cuda")
+else:
+    DEVICE = torch.device("cpu")
 
 # for plotting
 plt.rcParams["figure.figsize"] = (10.0, 8.0)  # set default size of plots
@@ -59,10 +63,10 @@ reset_seed(0)
 
 def get_data():
   train_dataset = PROPSDetectionDataset(
-      GOOGLE_DRIVE_PATH, "train", image_size=IMAGE_SHAPE[0],
+      PATH, "train", image_size=IMAGE_SHAPE[0],
       download=False  # True (for the first time)
   )
-  val_dataset = PROPSDetectionDataset(GOOGLE_DRIVE_PATH, "val", image_size=IMAGE_SHAPE[0])
+  val_dataset = PROPSDetectionDataset(PATH, "val", image_size=IMAGE_SHAPE[0])
   # `pin_memory` speeds up CPU-GPU batch transfer, `num_workers=NUM_WORKERS` loads data
   # on the main CPU process, suitable for Colab.
   train_loader = torch.utils.data.DataLoader(
